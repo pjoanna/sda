bd96517437ad17c4a06beea324ee2400b7950410
pjoanna <joannaplaga@gmail.com>
2017-08-03 20:48:45 +0200

    formularz wersja końcowa

--- FilesFix/src/Main.java
+++ FilesFix/src/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+       // new Main().init();
         System.out.println("Zaczynamy.");
         File file = new File("test.txt");
         Scanner scanner = new Scanner(System.in);
