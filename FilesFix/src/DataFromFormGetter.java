631ebfc37be376467a260c5a7dd07c5ea999692f
pjoanna <joannaplaga@gmail.com>
2017-08-03 23:27:40 +0200

    Usunięcie powtwórzeń w metodach

--- FilesFix/src/DataFromFormGetter.java
+++ FilesFix/src/DataFromFormGetter.java
@@ -6,8 +6,8 @@ import java.io.IOException;
  */
 public class DataFromFormGetter {
 
-    private Form form;
-    private BufferedReader reader;
+    private final Form form;
+    private final BufferedReader reader;
 
     public DataFromFormGetter(Form form, BufferedReader reader) {
         this.form = form;
@@ -15,53 +15,24 @@ public class DataFromFormGetter {
     }
 
     public String getAnswersFromForm() throws IOException {
-        getNameFromForm();
-        getSurnameFromForm();
-        getAgeFromForm();
-        getSexFromForm();
-        getAddedAnswers();
+        form.setName(nextValue());
+        form.setSurname(nextValue());
+        form.setAge(nextInt());
+        form.setSex(nextValue());
+        form.setAnswer1(nextValue());
+        form.setAnswer2(nextValue());
+        form.setAnswer3(nextValue());
         return form.getName() + form.getSurname();
     }
 
-    private void getAddedAnswers() throws IOException {
-        getAnswer1FromForm();
-        getAnswer2FromForm();
-        getAnswer3FromForm();
+    private int nextInt() throws IOException {
+        return Integer.parseInt(nextValue());
     }
 
-    private void getAnswer3FromForm() throws IOException {
-        String[] ans3 = reader.readLine().split("=");
-        form.setAnswer3(ans3[1]);
+    private String nextValue() throws IOException {
+        String[] ans = reader.readLine().split("=");
+        return ans[1];
     }
 
-    private void getAnswer2FromForm() throws IOException {
-        String[] ans2 = reader.readLine().split("=");
-        form.setAnswer2(ans2[1]);
-    }
 
-    private void getAnswer1FromForm() throws IOException {
-        String[] ans1 = reader.readLine().split("=");
-        form.setAnswer1(ans1[1]);
-    }
-
-    private void getSexFromForm() throws IOException {
-        String[] sexes = reader.readLine().split("=");
-        form.setSex(sexes[1]);
-    }
-
-    private void getAgeFromForm() throws IOException {
-        String[] ageString = reader.readLine().split("=");
-        Integer age = Integer.parseInt(ageString[1]);
-        form.setAge(age);
-    }
-
-    private void getSurnameFromForm() throws IOException {
-        String[] surnames = reader.readLine().split("=");
-        form.setSurname(surnames[1]);
-    }
-
-    private void getNameFromForm() throws IOException {
-        String[] names = reader.readLine().split("=");
-        form.setName(names[1]);
-    }
 }
